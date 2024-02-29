34f3da54f17a0414f9ee7212b3351c43fea9d84e
Naomicoder <94610429+Naomicoder@users.noreply.github.com>
2024-02-29 13:08:12 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+dere=input("Artifex sum nuper audire ")
+pre=int(input("temere specifica numerus 1 "))
+tty=int(input("temere specifica numerus 2 "))
+die="fprcfrohjdaseg,hk"
+meine="01010111 01010010 01001111 01001110 01000111"
+def code(gas, tro, it, se, vinil):
+    print(die)
+    if tro==(gas^3)*(1)*(gas*6)^2*(gas):
+        bil=se+it
+        if vinil=="c":
+            return("https://docs.google.com/document/d/166RTkS8pf9IB_Q6THu8l4ZiTuy3wkS1UG12Z7cGt06I/edit")
+        else:
+            return(bil)
+print(code(pre, tty, die, meine, dere))
